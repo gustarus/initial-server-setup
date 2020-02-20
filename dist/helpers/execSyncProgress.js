d99381355fdfb615dea7983dae5e1a9a78fe249a
Pavel Kondratenko <iam@kondratenko.me>
2020-02-20 07:15:56 +0300

    remove unused helper

--- dist/helpers/execSyncProgress.js
+++ dist/helpers/execSyncProgress.js
@@ -6,11 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const safe_1 = __importDefault(require("colors/safe"));
 const createCommand_1 = __importDefault(require("./createCommand"));
-const secretCommandVariables_1 = __importDefault(require("./secretCommandVariables"));
 function execSyncProgress(parts, scenario) {
     const command = createCommand_1.default(parts);
     const compiled = command.compile();
-    console.log(`$ ${secretCommandVariables_1.default(compiled)}`);
+    console.log(`$ ${compiled}`);
     try {
         switch (scenario) {
             case 'display':
@@ -22,7 +21,7 @@ function execSyncProgress(parts, scenario) {
         }
     }
     catch (error) {
-        console.log(safe_1.default.red(secretCommandVariables_1.default(error.stack)));
+        console.log(safe_1.default.red(error.stack));
         process.exit(1);
         return false;
     }
--- src/helpers/execSyncProgress.ts
+++ src/helpers/execSyncProgress.ts
@@ -1,14 +1,13 @@
 import { execSync } from 'child_process';
 import colors from 'colors/safe';
 import createCommand from './createCommand';
-import secretCommandVariables from './secretCommandVariables';
 import { CommandSpace } from '../models/Command';
 
 export default function execSyncProgress(parts: CommandSpace.Part[], scenario: 'display' | 'return'): any {
   const command = createCommand(parts);
   const compiled = command.compile();
 
-  console.log(`$ ${secretCommandVariables(compiled)}`);
+  console.log(`$ ${compiled}`);
 
   try {
     switch (scenario) {
@@ -20,7 +19,7 @@ export default function execSyncProgress(parts: CommandSpace.Part[], scenario: '
         throw new Error('Invalid scenario passed');
     }
   } catch (error) {
-    console.log(colors.red(secretCommandVariables(error.stack)));
+    console.log(colors.red(error.stack));
     process.exit(1);
     return false;
   }
